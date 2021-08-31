ae0bc2a2adc8033c92e19626e37aec4f607f351d
Rick <71887046+anayaricardo@users.noreply.github.com>
2021-08-31 10:39:49 -0500

    Contact me Einstein.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import Ein from './Ein.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={Ein} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi! My name is Rick I'm a <code>Computer Systems Engineer</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/ricardoanaya/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contact me
         </a>
       </header>
     </div>
