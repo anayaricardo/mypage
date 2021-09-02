002e9348934422b47292df56c045aebc50fd9e3d
Rick <71887046+anayaricardo@users.noreply.github.com>
2021-09-01 20:39:45 -0500

    Añadí imagen y cambié los colores

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import Ein from './Ein.png';
+import Luz from './chip.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <script src="https://cdn.sift.com/s.js"></script>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src={Ein} className="App-logo" alt="logo" />
+        <img src={Luz} className="App-logo" alt="logo" />
         <p>
-          Hi! My name is Rick I'm a <code>Computer Systems Engineer</code>
+          Hi! My name is Rick and I'm a <code>Computer Systems Engineer</code>
         </p>
         <a
           className="App-link"
@@ -17,6 +18,30 @@ function App() {
           rel="noopener noreferrer"
         >
           Contact me
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/anayaricardo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          className="App-link"
+          href="https://twitter.com/ricky_anaya"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+        <a
+          className="App-link"
+          href="https://tic-tac-toc-react-game.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tic-Tac-Toe
         </a>
       </header>
     </div>
