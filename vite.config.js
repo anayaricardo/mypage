fd2bf4652f6f6f7d159fe12063ecea17490d0bc0
Ricardo Anaya <einstein.letsgetrelative@gmail.com>
2026-03-05 02:15:31 -0600

    Vite config /

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/mypage/",
   server: {
     host: 'localhost',
     port: 5173,
